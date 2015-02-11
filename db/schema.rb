59811cad26a48a724d703af2f89e0e14b89f38f5
Vlad Marinov <vladmrnv@gmail.com>
2015-02-10 21:17:40 -0800

    Added users and connection from users to notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211045736) do
+ActiveRecord::Schema.define(version: 20150211045934) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
