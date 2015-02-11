fe7269e81b5efc12aa5c334d6397f51b8cc8b55e
Vlad Marinov <vladmrnv@gmail.com>
2015-02-10 20:06:08 -0800

    Updated notes controller && simple form

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,7 @@
 class NotesController < ApplicationController
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
   def index
-  
+    @notes = Note.all.order("created_at DESC")
   end
 
   def show
@@ -8,12 +9,17 @@ class NotesController < ApplicationController
   end
 
   def new
-
+    @note = Note.new
   end
 
   def create
-
-  end
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
+  end 
 
   def edit
 
@@ -29,10 +35,10 @@ class NotesController < ApplicationController
 
   private
   def find_note
-
+    @note = Note.find(params[:id])
   end
 
   def note_params
-
+    params.require(:note).permit(:title, :message)
   end
 end
